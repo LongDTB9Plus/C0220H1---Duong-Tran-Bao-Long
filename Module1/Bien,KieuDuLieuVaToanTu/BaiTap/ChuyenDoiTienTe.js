cb5771335775004fc294a2c7fb7e57cec88bfe46
BaoLongDNH01 <duongtranbaolongdn@gmail.com>
2020-03-02 08:11:50 +0700

    'BaiTap'

--- Module1/Bien,KieuDuLieuVaToanTu/BaiTap/ChuyenDoiTienTe.js
+++ Module1/Bien,KieuDuLieuVaToanTu/BaiTap/ChuyenDoiTienTe.js
@@ -1,16 +1,18 @@
 function process() {
-    var beforeConvert = document.getElementById("Amount");
+    var beforeConvert = document.getElementById("amount");
     var fCurrency = document.getElementById("fromCurrency");
     var tCurrency = document.getElementById("toCurrency");
     var afterConvert;
 
 
-    if (fCurrency.value == tCurrency.value) {
-    document.write("Result:" + beforeConvert);
-    }else if(fCurrency.value == "VND"){
-            afterConvert = (parseInt(beforeConvert.value)*23000);
-        }else if (fCurrency.value == "USD"){
+    if (fCurrency.value === tCurrency.value) {
+    document.write("Result:" + beforeConvert.value);
+    }else if(fCurrency.value === "VND"){
             afterConvert = (parseInt(beforeConvert.value)/23000);
+             document.write("Result:" + afterConvert);
+        }else if (fCurrency.value === "USD"){
+            afterConvert = (parseInt(beforeConvert.value)*23000);
+            document.write("Result:" + afterConvert);
         }
-    document.write("Result:" + afterConvert);
+
 }
---
+++ Module1/Bien,KieuDuLieuVaToanTu/BaiTap/MayTinh4Ham.js
@@ -0,0 +1,5 @@
+function process(ope) {
+    var a =document.getElementById("num1");
+    var b =document.getElementById("num2");
+document.write("Result:" + eval(a.value + ope + b.value));
+}
