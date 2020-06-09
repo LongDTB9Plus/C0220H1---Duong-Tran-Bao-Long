dcdbc873e41031e9f935e949dc7be4f6b281c429
BaolongDNH01 <61400179+BaolongDNH01@users.noreply.github.com>
2020-06-09 19:31:14 +0700

    bt

---
+++ Module2/Web/BaiTap/untitled/src/test.java
@@ -0,0 +1,8 @@
+public class test {
+    public static void main(String[] args) {
+        String a = "";
+        String b = "";
+        System.out.println(a.length());
+        System.out.println(b.isEmpty());
+    }
+}
